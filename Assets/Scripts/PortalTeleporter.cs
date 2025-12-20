c41ef003351a2b763c490ca395cd575fd260f3ae
s3888582 <s388852@student.rmit.edu.au>
2025-12-20 22:13:24 +1100

    added a two-way portal, has a collision bug tho

---
+++ Assets/Scripts/PortalTeleporter.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalTeleporter : MonoBehaviour {
+
+    public Transform player;
+    public Transform reciever;
+
+    private bool playerIsOverlapping = false; // player is in contact w portal face
+
+    void Update() {
+        if (playerIsOverlapping) {
+            Vector3 portalToPlayer = player.position - transform.position;
+            float dotProduct = Vector3.Dot(transform.up, portalToPlayer);
+
+            if (dotProduct < 0f) {
+                float rotationDiff = Quaternion.Angle(transform.rotation, reciever.rotation);
+                rotationDiff += 180;
+                player.Rotate(Vector3.up, rotationDiff);
+
+                Vector3 positionOffset = Quaternion.Euler(0f, rotationDiff, 0f) * portalToPlayer;
+                player.position = reciever.position + positionOffset;
+
+                playerIsOverlapping = false;
+            }
+        }
+    }
+
+    void OnTriggerEnter(Collider other) {
+        if (other.tag == "Player") {
+            playerIsOverlapping = true;
+        }
+    }
+
+    void onTriggerExit(Collider other) {
+        if (other.tag == "Player") {
+            playerIsOverlapping = false;
+        }
+    }
+}
---
+++ Assets/Scripts/PortalTextureSetup.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalTextureSetup : MonoBehaviour {
+
+    public Camera cameraA;
+    public Camera cameraB;
+
+    public Material cameraMatA;
+    public Material cameraMatB;
+
+    void Start() {
+        if (cameraA.targetTexture != null) {
+            cameraA.targetTexture.Release();
+        }
+        cameraA.targetTexture = new RenderTexture(Screen.width, Screen.height, 24); // 24 implies depth
+        cameraMatA.mainTexture = cameraA.targetTexture;
+
+        if (cameraB.targetTexture != null) {
+            cameraB.targetTexture.Release();
+        }
+        cameraB.targetTexture = new RenderTexture(Screen.width, Screen.height, 24); // 24 implies depth
+        cameraMatB.mainTexture = cameraB.targetTexture;
+    }
+}
